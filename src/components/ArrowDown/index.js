3bbac96bb0e4c939a36e12bbe3f38ace7413e0e4
Lucas Meira <lucaas.meiraf@gmail.com>
2022-02-01 14:36:46 -0300

    :lipstick: Add ArrowDown CSS with animation

--- src/components/ArrowDown/index.js
+++ src/components/ArrowDown/index.js
@@ -1,5 +1,10 @@
-export default function ArrowDown(){
+import "./style.css";
+
+export default function ArrowDown(currentPlayer = 1){
     return /* html */ `
-    <img src="images/IconArrowDown.svg" alt="Icone da seta" />
+    <img class="arrow-down"
+        data-currentPlayer="${currentPlayer}"
+         src="images/IconArrowDown.svg" 
+         alt="Icone da seta" />
     `;
 }
