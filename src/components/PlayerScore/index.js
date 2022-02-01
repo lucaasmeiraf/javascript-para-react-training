483c436e130411ec05d06c7feacc1f408c27deb3
Lucas Meira <lucaas.meiraf@gmail.com>
2022-02-01 01:50:50 -0300

    :lipstick: Create styles of PlayerScore

---
+++ src/components/PlayerScore/index.js
@@ -0,0 +1,11 @@
+import "./style.css"
+
+export default function PlayerScore(points = 0){
+    return /*html*/`
+    <ol class="player-score" data-points = ${points}>
+        <li class="pointer">Um</li>
+        <li class="pointer">Dois</li>
+        <li class="pointer">Tres</li>
+    </ol>
+    `
+}
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,10 +1,15 @@
 import PlayerName from "../../components/PlayerName";
+import PlayerScore from "../../components/PlayerScore";
+import VsPlayer from "../../components/VsPlayer";
 import "./style.css";
 
 function ScoreBoard(){
     return /*html*/`
     <header class="score-board">
         ${PlayerName('Player1')}
+        ${PlayerScore(2)}
+        ${VsPlayer()}
+        ${PlayerScore(1)}
         ${PlayerName('Player2')}
     </header>
     `;
